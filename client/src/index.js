bb06840db76c2bb565c61b77c810c59609f87882
Konstantin Harutyunyan <harutyunyankostya@yahoo.com>
2018-06-01 13:28:01 +0400

    Set up webpack, redux, eslint and other necessary packages

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import store, { history } from './store'
+import App from './containers/app'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const target = document.querySelector('#root')
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  target
+)
