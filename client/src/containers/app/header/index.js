da01e64621f3608ef316a9d5021bc30313abd53d
Konstantin Harutyunyan <harutyunyankostya@yahoo.com>
2018-08-15 16:36:18 +0400

    - Navbar sticks to top of screen - Sections highlight when scrolling to them. - When clicking on Services, Team, Contact Us, it takes you to that section on the page

--- client/src/containers/app/header/index.js
+++ client/src/containers/app/header/index.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
-import { Col, Collapse, Navbar, NavbarToggler } from 'reactstrap'
+import { Collapse, Navbar, NavbarToggler } from 'reactstrap'
 
 import './index.css'
+
+import Scrollspy from 'react-scrollspy'
 
 export default class Header extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      collapsed: true
+      collapsed: true,
+      currentSection: 'home'
     };
     this.toggleNavbar = this.toggleNavbar.bind(this)
+    this.scrollToSection = this.scrollToSection.bind(this)
   }
 
   toggleNavbar(value) {
@@ -21,21 +25,28 @@ export default class Header extends Component {
     })
   }
 
+  scrollToSection(event, sectionName) {
+    const scrollNode = document.getElementById(sectionName)
+    setTimeout(() => window.scrollTo(0, !!scrollNode ? scrollNode.offsetTop : 0), !event ? 500 : 0)
+    this.toggleNavbar(true)
+  }
+
   render() {
     return (
       <Navbar className="headerNavBarContainer navbar navbar-expand-lg navbar-light fixed-top">
         <div ref="headerContainer" className="contentContainer">
+
           <NavLink to="/" className="headerLogo">
             <img src="/assets/images/headerLogo.png" alt="logo"/>
           </NavLink>
           <NavbarToggler onClick={this.toggleNavbar} className='headerNavBarToggler'/>
           <Collapse isOpen={!this.state.collapsed} navbar className="headerNavBar">
-            <Col xl={8} lg={8} className="navbar-nav headerCenterMenuItemsContainer mr-auto">
-              <NavLink to="/" exact={true} onClick={() => this.toggleNavbar(true)} className="headerMenuItem nav-item nav-link">Home</NavLink>
-              <NavLink to="/services" exact={true} onClick={() => this.toggleNavbar(true)} className="headerMenuItem nav-item nav-link">Services</NavLink>
-              <NavLink to="/team" exact={true} onClick={() => this.toggleNavbar(true)} className="headerMenuItem nav-item nav-link">Team</NavLink>
-              <NavLink to="/contactUs" exact={true} onClick={() => this.toggleNavbar(true)} className="headerMenuItem nav-item nav-link">Contact Us</NavLink>
-            </Col>
+            <Scrollspy componentTag='div' items={[ 'home', 'services', 'team', 'contact' ]} currentClassName="active" className="col-xl-8 col-lg-8 navbar-nav headerCenterMenuItemsContainer mr-auto">
+              <a href="#home" onClick={() => this.toggleNavbar(true)} className="headerMenuItem nav-item nav-link">Home</a>
+              <a href="#services" onClick={() => this.toggleNavbar(true)} className="headerMenuItem nav-item nav-link">Services</a>
+              <a href="#team" onClick={() => this.toggleNavbar(true)} className="headerMenuItem nav-item nav-link">Team</a>
+              <a href="#contact" onClick={() => this.toggleNavbar(true)} className="headerMenuItem nav-item nav-link">Contact Us</a>
+            </Scrollspy>
           </Collapse>
         </div>
       </Navbar>
--- client/src/containers/home/index.js
+++ client/src/containers/home/index.js
@@ -10,7 +10,9 @@ class Home extends Component {
   render() {
     return (
       <div className="homeContainer">
-        <ImageSlider/>
+        <section id="home">
+          <ImageSlider/>
+        </section>
         <Col className="homePageSectionContainer">
           <p className="homePageSectionHeader">What is Spark Lab?</p>
           <div className="homePageSectionUnderHeaderLine"/>
@@ -20,63 +22,69 @@ class Home extends Component {
             </Col>
           </div>
         </Col>
-        <Col className="homePageSectionContainer homePageServicesSectionContainer bg-white">
-          <p className="homePageSectionHeader">Services</p>
-          <div className="homePageSectionUnderHeaderLine"/>
-          <div className="contentContainer">
-            <Col xl={{ size: 12, offset: 0 }} lg={{ size: 12, offset: 0 }} md={{ size: 12, offset: 0 }} sm={{ size: 12, offset: 0 }} xs={{ size: 12, offset: 0 }} className="homePageSectionContentPartContainer">
-              <Col xl={{ size: 4, offset: 0 }} className='d-flex flex-column align-items-center servicePointContainer'>
-                <img src='/assets/images/research.png' alt="Research" className="servicePointIcon"/>
-                <p className="servicePointHeader">Research</p>
-                <p className="servicePointDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-              </Col>
-              <Col xl={{ size: 4, offset: 0 }} className='d-flex flex-column align-items-center servicePointContainer'>
-                <img src='/assets/images/clipboard.png' alt="Research" className="servicePointIcon"/>
-                <p className="servicePointHeader">Requirements</p>
-                <p className="servicePointDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-              </Col>
-              <Col xl={{ size: 4, offset: 0 }} className='d-flex flex-column align-items-center servicePointContainer'>
-                <img src='/assets/images/vector.png' alt="Research" className="servicePointIcon"/>
-                <p className="servicePointHeader">Design</p>
-                <p className="servicePointDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-              </Col>
-              <Col xl={{ size: 4, offset: 0 }} className='d-flex flex-column align-items-center servicePointContainer'>
-                <img src='/assets/images/cogwheel.png' alt="Research" className="servicePointIcon"/>
-                <p className="servicePointHeader">Implementation</p>
-                <p className="servicePointDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-              </Col>
-              <Col xl={{ size: 4, offset: 0 }} className='d-flex flex-column align-items-center servicePointContainer'>
-                <img src='/assets/images/brain.png' alt="Research" className="servicePointIcon"/>
-                <p className="servicePointHeader">Management</p>
-                <p className="servicePointDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-              </Col>
-            </Col>
-          </div>
-        </Col>
-        <Col className="homePageSectionContainer">
-          <p className="homePageSectionHeader">Team</p>
-          <div className="homePageSectionUnderHeaderLine"/>
-          <div className="contentContainer">
-            <Col xl={{ size: 8, offset: 2 }} className="homePageSectionContentPartContainer">
-              <p className="teamPartText">The Spark Lab team of analysts, software architects, and system engineers is comprised of IT management veterans, operations specialists, and other industry experts. Our team is ready to engage with companies ready to take their business to the next level.</p>
-              <Col xl={{ size: 12, offset: 0 }} className="d-flex flex-wrap">
-                <Col className="d-flex flex-column align-items-center">
-                  <div className="teamMemberAvatar ronAvatar"/>
-                  <p className="teamMemberName">Ron Parnaso</p>
+        <section id="services">
+          <Col className="homePageSectionContainer homePageServicesSectionContainer bg-white" id="services">
+            <p className="homePageSectionHeader">Services</p>
+            <div className="homePageSectionUnderHeaderLine"/>
+            <div className="contentContainer">
+              <Col xl={{ size: 12, offset: 0 }} lg={{ size: 12, offset: 0 }} md={{ size: 12, offset: 0 }} sm={{ size: 12, offset: 0 }} xs={{ size: 12, offset: 0 }} className="homePageSectionContentPartContainer">
+                <Col xl={{ size: 4, offset: 0 }} className='d-flex flex-column align-items-center servicePointContainer'>
+                  <img src='/assets/images/research.png' alt="Research" className="servicePointIcon"/>
+                  <p className="servicePointHeader">Research</p>
+                  <p className="servicePointDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
                 </Col>
-                <Col className="d-flex flex-column align-items-center">
-                  <div className="teamMemberAvatar randyAvatar"/>
-                  <p className="teamMemberName">Randy Hamilton</p>
+                <Col xl={{ size: 4, offset: 0 }} className='d-flex flex-column align-items-center servicePointContainer'>
+                  <img src='/assets/images/clipboard.png' alt="Research" className="servicePointIcon"/>
+                  <p className="servicePointHeader">Requirements</p>
+                  <p className="servicePointDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                </Col>
+                <Col xl={{ size: 4, offset: 0 }} className='d-flex flex-column align-items-center servicePointContainer'>
+                  <img src='/assets/images/vector.png' alt="Research" className="servicePointIcon"/>
+                  <p className="servicePointHeader">Design</p>
+                  <p className="servicePointDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                </Col>
+                <Col xl={{ size: 4, offset: 0 }} className='d-flex flex-column align-items-center servicePointContainer'>
+                  <img src='/assets/images/cogwheel.png' alt="Research" className="servicePointIcon"/>
+                  <p className="servicePointHeader">Implementation</p>
+                  <p className="servicePointDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                </Col>
+                <Col xl={{ size: 4, offset: 0 }} className='d-flex flex-column align-items-center servicePointContainer'>
+                  <img src='/assets/images/brain.png' alt="Research" className="servicePointIcon"/>
+                  <p className="servicePointHeader">Management</p>
+                  <p className="servicePointDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
                 </Col>
               </Col>
-            </Col>
-          </div>
-        </Col>
-        <Col className="homePageSectionContainer bg-white">
-          <p className="homePageSectionHeader">Request a consulting session</p>
-          <div className="homePageSectionUnderHeaderLine"/>
-          <RequestForm/>
-        </Col>
+            </div>
+          </Col>
+        </section>
+        <section id="team">
+          <Col className="homePageSectionContainer">
+            <p className="homePageSectionHeader">Team</p>
+            <div className="homePageSectionUnderHeaderLine"/>
+            <div className="contentContainer">
+              <Col xl={{ size: 8, offset: 2 }} className="homePageSectionContentPartContainer">
+                <p className="teamPartText">The Spark Lab team of analysts, software architects, and system engineers is comprised of IT management veterans, operations specialists, and other industry experts. Our team is ready to engage with companies ready to take their business to the next level.</p>
+                <Col xl={{ size: 12, offset: 0 }} className="d-flex flex-wrap">
+                  <Col className="d-flex flex-column align-items-center">
+                    <div className="teamMemberAvatar ronAvatar"/>
+                    <p className="teamMemberName">Ron Parnaso</p>
+                  </Col>
+                  <Col className="d-flex flex-column align-items-center">
+                    <div className="teamMemberAvatar randyAvatar"/>
+                    <p className="teamMemberName">Randy Hamilton</p>
+                  </Col>
+                </Col>
+              </Col>
+            </div>
+          </Col>
+        </section>
+        <section id="contact">
+          <Col className="homePageSectionContainer bg-white">
+            <p className="homePageSectionHeader">Request a consulting session</p>
+            <div className="homePageSectionUnderHeaderLine"/>
+            <RequestForm/>
+          </Col>
+        </section>
       </div>
     )
   }
